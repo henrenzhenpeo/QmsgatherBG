f0d5a9f78993b18d354cc6f681553368df3f92b2
henrenzhenpeo <115695966+henrenzhenpeo@users.noreply.github.com>
2024-12-10 09:53:53 +0800

    Add files via upload

---
+++ Qmsgather_BG/src/test/java/com/biel/qmsgather/QmsgatherBgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biel.qmsgather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QmsgatherBgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
