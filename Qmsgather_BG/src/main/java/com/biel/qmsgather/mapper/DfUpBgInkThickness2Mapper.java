c513d98701ef04201ea913629652f1fc07e72300
chenbo <849515805@qq.com>
2025-02-27 16:31:30 +0800

    DfUpBgInkThickness2代码更新-2025-02-27-chenbo

---
+++ Qmsgather_BG/src/main/java/com/biel/qmsgather/mapper/DfUpBgInkThickness2Mapper.java
@@ -0,0 +1,20 @@
+package com.biel.qmsgather.mapper;
+
+import com.biel.qmsgather.domain.DfUpBgInkThickness2;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+* @author dafenqi
+* @description 针对表【df_up_bg_ink_thickness2(bg油墨厚度)】的数据库操作Mapper
+* @createDate 2025-02-27 16:28:07
+* @Entity com.biel.qmsgather.domain.DfUpBgInkThickness2
+*/
+@Mapper
+public interface DfUpBgInkThickness2Mapper extends BaseMapper<DfUpBgInkThickness2> {
+
+}
+
+
+
+
---
+++ Qmsgather_BG/src/main/java/com/biel/qmsgather/service/impl/DfUpBgInkThickness2ServiceImpl.java
@@ -0,0 +1,24 @@
+package com.biel.qmsgather.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.biel.qmsgather.domain.DfUpBgInkThickness2;
+import com.biel.qmsgather.service.DfUpBgInkThickness2Service;
+import com.biel.qmsgather.mapper.DfUpBgInkThickness2Mapper;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+* @author dafenqi
+* @description 针对表【df_up_bg_ink_thickness2(bg油墨厚度)】的数据库操作Service实现
+* @createDate 2025-02-27 16:28:07
+*/
+@Service
+@Transactional
+public class DfUpBgInkThickness2ServiceImpl extends ServiceImpl<DfUpBgInkThickness2Mapper, DfUpBgInkThickness2>
+    implements DfUpBgInkThickness2Service{
+
+}
+
+
+
+
